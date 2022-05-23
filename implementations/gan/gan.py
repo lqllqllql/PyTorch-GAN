88c4880429902d5002d8b45e7e88706a4b1fa4ae
lqllqllql <103975573+lqllqllql@users.noreply.github.com>
2022-05-23 20:32:57 +0800

    Update gan.py

--- implementations/gan/gan.py
+++ implementations/gan/gan.py
@@ -84,10 +84,10 @@ class Discriminator(nn.Module):
         return validity
 
 # 实例化模型
-model = Generator()
+#model = Generator()
 
 # 将模型写入tensorboard
-tb_writer.add_graph(model)
+#tb_writer.add_graph(model)
     
 # Loss function
 adversarial_loss = torch.nn.BCELoss()
